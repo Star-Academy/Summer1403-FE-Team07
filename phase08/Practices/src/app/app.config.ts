8e24c35cd31386f8bb0fd9c12ac5adfcb5f4ce9a
ArashAzarpoor <azarpoor81@gmail.com>
2024-08-05 22:57:57 +0330

    Remove duplicate files

--- phase08/Practices/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import routeConfig from './routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    routeConfig,
-  ],
-};

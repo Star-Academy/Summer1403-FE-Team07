fbb0530e784da0db1dc0e800b06faab048d5d3eb
ArashAzarpoor <azarpoor81@gmail.com>
2024-08-07 15:59:15 +0330

    Add navbar unit test

---
+++ phase11/book-store/jest.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default {
+  displayName: {
+    name: 'Example App',
+    color: 'blue',
+  },
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+};
--- phase11/book-store/src/app/components/navbar/navbar.component.spec.ts
+++ phase11/book-store/src/app/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import {MessageService} from "primeng/api";
+import {By} from "@angular/platform-browser";
+import {screen} from "@testing-library/angular";
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,10 +11,11 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [NavbarComponent],
+      providers: [MessageService]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +24,36 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it ('should change theme colors when theme button is clicked', () => {
+    const themeButton = fixture.debugElement.query(By.css('[data-testid="theme-button"]'));
+    const image = themeButton.query(By.css('img'));
+
+    themeButton.nativeElement.click();
+
+    fixture.detectChanges();
+
+    expect(image.nativeElement.src).toContain('moon');
+  })
+
+  it ('should change theme class when theme button is clicked', () => {
+    const themeButton = fixture.debugElement.query(By.css('[data-testid="theme-button"]'));
+
+    themeButton.nativeElement.click();
+
+    fixture.detectChanges();
+  })
+
 });
+
+// beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       providers: [MessageService],
+//     }).compileComponents();
+//   })
+//
+//   test('should render navbar', async () => {
+//     await render(NavbarComponent)
+//
+//     expect(screen).toBeInTheDocument();
+//   })

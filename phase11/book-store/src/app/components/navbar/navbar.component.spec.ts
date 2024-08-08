220611553a4a076cc0b49910a99c9ee607299577
ArashAzarpoor <azarpoor81@gmail.com>
2024-08-08 13:22:22 +0330

    Add navbar tests

--- phase11/book-store/src/app/components/navbar/navbar.component.spec.ts
+++ phase11/book-store/src/app/components/navbar/navbar.component.spec.ts
@@ -1,9 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NavbarComponent } from './navbar.component';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {NavbarComponent} from './navbar.component';
 import {MessageService} from "primeng/api";
 import {By} from "@angular/platform-browser";
-import {screen} from "@testing-library/angular";
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -14,7 +12,7 @@ describe('NavbarComponent', () => {
       imports: [NavbarComponent],
       providers: [MessageService]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
@@ -25,25 +23,33 @@ describe('NavbarComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it ('should change theme colors when theme button is clicked', () => {
+  it('should change theme colors when theme button is clicked', () => {
+    // Arrange
     const themeButton = fixture.debugElement.query(By.css('[data-testid="theme-button"]'));
     const image = themeButton.query(By.css('img'));
 
-    themeButton.nativeElement.click();
-
+    // Act
+    themeButton.triggerEventHandler('click');
     fixture.detectChanges();
 
+    // Assert
     expect(image.nativeElement.src).toContain('moon');
   })
 
-  it ('should change theme class when theme button is clicked', () => {
+  it('should change theme class when theme button is clicked', fakeAsync(() => {
     const themeButton = fixture.debugElement.query(By.css('[data-testid="theme-button"]'));
 
-    themeButton.nativeElement.click();
-
+    themeButton.triggerEventHandler('click', null);
     fixture.detectChanges();
-  })
 
+    themeButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const newThemeButtonContainer = fixture.debugElement.query(By.css('[data-testid="theme-button"]'));
+    const newThemeButton = newThemeButtonContainer.query(By.css('.theme-container'));
+
+    expect(getComputedStyle(newThemeButton.nativeElement).backgroundColor).toBe('rgb(245, 245, 255)');
+  }));
 });
 
 // beforeEach(async () => {

316a70c7aa5e13d5b6f31731e393abbb1ccce543
ArashAzarpoor <azarpoor81@gmail.com>
2024-08-08 15:13:48 +0330

    Fix navbar component

---
+++ phase09/book-store/src/app/components/navbar-button/navbar-button.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, ViewEncapsulation} from '@angular/core';
+import {Button} from "primeng/button";
+import {NgOptimizedImage} from "@angular/common";
+import {ButtonData} from "../../models/ButtonData";
+
+@Component({
+  selector: 'app-navbar-button',
+  standalone: true,
+  imports: [
+    Button,
+    NgOptimizedImage
+  ],
+  templateUrl: './navbar-button.component.html',
+  styleUrl: './navbar-button.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class NavbarButtonComponent {
+  @Input() buttonDate: ButtonData = {
+    containerClass: '',
+    imageSrc: '',
+    alt: '',
+    height: 0,
+    width: 0
+  };
+}
--- phase09/book-store/src/app/components/navbar/navbar.component.ts
+++ phase09/book-store/src/app/components/navbar/navbar.component.ts
@@ -13,7 +13,6 @@ import {NavigationEnd, Router} from "@angular/router";
 import {Button} from "primeng/button";
 import {DialogModule} from 'primeng/dialog';
 import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ToastModule} from "primeng/toast";
 import {CalendarModule} from "primeng/calendar";
 import {InputNumberModule} from "primeng/inputnumber";
 import {SearchComponent} from "../search/search.component";
@@ -21,6 +20,8 @@ import {debounceTime, distinctUntilChanged, filter, switchMap} from "rxjs/operat
 import {ThemeService} from "../../services/theme/theme.service";
 import {BookModalComponent} from "../book-modal/book-modal.component";
 import {BookSearchService} from "../../services/search/book-search.service";
+import {NavbarButtonComponent} from "../navbar-button/navbar-button.component";
+import {ButtonData} from "../../models/ButtonData";
 
 @Component({
   selector: 'app-navbar',
@@ -31,7 +32,6 @@ import {BookSearchService} from "../../services/search/book-search.service";
     DialogModule,
     ReactiveFormsModule,
     NgIf,
-    ToastModule,
     CalendarModule,
     InputNumberModule,
     NgTemplateOutlet,
@@ -43,20 +43,41 @@ import {BookSearchService} from "../../services/search/book-search.service";
     NgForOf,
     BookModalComponent,
     FormsModule,
+    NavbarButtonComponent,
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
-  encapsulation: ViewEncapsulation.None
+  styleUrl: './navbar.component.scss'
 })
 
 export class NavbarComponent implements OnInit {
   visible: boolean = false;
   searchControl = new FormControl();
   isLight: boolean = false;
-  themeSrc: string = '/icons/dark/sun.svg';
-  favoriteSrc: string = '/icons/dark/heart-dark.svg';
-  addSrc: string = '/icons/dark/add-dark.svg';
   searchSrc: string = '/icons/dark/search-dark.svg';
+
+  bookAddButton: ButtonData = {
+    containerClass: 'add-container',
+    imageSrc: '/icons/dark/add-dark.svg',
+    alt: 'profile',
+    height: 50,
+    width: 50
+  };
+
+  favoriteButton: ButtonData = {
+    containerClass: 'favorite-container',
+    imageSrc: '/icons/dark/heart-dark.svg',
+    alt: 'profile',
+    height: 50,
+    width: 50
+  };
+
+  themeButton: ButtonData = {
+    containerClass: 'theme-container',
+    imageSrc: '/icons/dark/sun.svg',
+    alt: 'profile',
+    height: 50,
+    width: 50
+  };
 
   constructor(
     private router: Router,
@@ -92,19 +113,19 @@ export class NavbarComponent implements OnInit {
     });
   }
 
-  showDialog() {
-    this.visible = true;
-  }
-
   closeDialog() {
     this.visible = false;
   }
 
+  showDialog() {
+    this.visible = true;
+  }
+
   changeTheme() {
     this.isLight = !this.isLight;
-    this.themeSrc = this.isLight ? '/icons/light/moon.svg' : '/icons/dark/sun.svg';
-    this.favoriteSrc = this.isLight ? '/icons/light/heart.svg' : '/icons/dark/heart-dark.svg';
-    this.addSrc = this.isLight ? '/icons/light/add.svg' : '/icons/dark/add-dark.svg';
+    this.themeButton.imageSrc = this.isLight ? '/icons/light/moon.svg' : '/icons/dark/sun.svg';
+    this.favoriteButton.imageSrc = this.isLight ? '/icons/light/heart.svg' : '/icons/dark/heart-dark.svg';
+    this.bookAddButton.imageSrc = this.isLight ? '/icons/light/add.svg' : '/icons/dark/add-dark.svg';
     this.searchSrc = this.isLight ? '/icons/light/search.svg' : '/icons/dark/search-dark.svg';
     this.themeService.toggleTheme(this.isLight);
   }

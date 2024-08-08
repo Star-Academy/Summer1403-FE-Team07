027e1f02bfaf7d83102687007aae60a938514e6c
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-08-08 20:10:24 +0330

    add unit test for genre books component

--- phase11/book-store/src/app/components/genre-books/genre-books.component.spec.ts
+++ phase11/book-store/src/app/components/genre-books/genre-books.component.spec.ts
@@ -1,23 +1,209 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GenreBooksComponent } from './genre-books.component';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { BookSearchService } from '../../services/search/book-search.service';
+import { ThemeService } from '../../services/theme/theme.service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { GenreBooks } from '../../models/GenreBooks';
+import { Subject } from 'rxjs';
+import { SearchType } from '../../models/SearchType';
 
-describe('BookCatListAllComponent', () => {
+describe('GenreBooksComponent', () => {
   let component: GenreBooksComponent;
   let fixture: ComponentFixture<GenreBooksComponent>;
+  let debugElement: DebugElement;
+  let mockLocation: jasmine.SpyObj<Location>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockBookSearchService: jasmine.SpyObj<BookSearchService>;
+  let mockThemeService: jasmine.SpyObj<ThemeService>;
+  let searchResultsSubject: Subject<SearchType>;
+  let themeToggleSubject: Subject<boolean>;
 
   beforeEach(async () => {
+    mockLocation = jasmine.createSpyObj('Location', ['back']);
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+    searchResultsSubject = new Subject<SearchType>();
+    themeToggleSubject = new Subject<boolean>();
+    mockBookSearchService = jasmine.createSpyObj('BookSearchService', [], {
+      searchResults$: searchResultsSubject.asObservable(),
+    });
+    mockThemeService = jasmine.createSpyObj('ThemeService', [], {
+      onToggle: themeToggleSubject.asObservable(),
+    });
+
     await TestBed.configureTestingModule({
-      imports: [GenreBooksComponent]
-    })
-    .compileComponents();
+      imports: [GenreBooksComponent],
+      providers: [
+        { provide: Location, useValue: mockLocation },
+        { provide: Router, useValue: mockRouter },
+        { provide: BookSearchService, useValue: mockBookSearchService },
+        { provide: ThemeService, useValue: mockThemeService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GenreBooksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    debugElement = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('SHOULD create the component WHEN initialized', () => {
+    // Arrange & Act (Component creation is handled by the framework)
+
+    // Assert
     expect(component).toBeTruthy();
+  });
+
+  it('SHOULD display the correct book details WHEN books input is set', () => {
+    // Arrange
+    const expectedBooks: GenreBooks = {
+      genreName: 'Fiction',
+      booksList: [
+        {
+          name: 'Test Book',
+          image: 'test-image.jpg',
+          genre: ['Fiction'],
+          author: 'Test Author',
+          publishData: '',
+          price: 20,
+        },
+      ],
+    };
+    component.books = expectedBooks;
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    const bookCardElements = debugElement.queryAll(By.css('app-book-card'));
+    expect(bookCardElements.length).toBe(1);
+    const genreNameElement = debugElement.query(
+      By.css('.genre-container__genre-name'),
+    );
+    expect(genreNameElement.nativeElement.textContent).toBe(
+      expectedBooks.genreName,
+    );
+  });
+
+  it('SHOULD navigate to book details WHEN book card is clicked', () => {
+    // Arrange
+    const expectedBooks: GenreBooks = {
+      genreName: 'Fiction',
+      booksList: [
+        {
+          name: 'Test Book',
+          image: 'test-image.jpg',
+          genre: ['Fiction'],
+          author: 'Test Author',
+          publishData: '',
+          price: 20,
+        },
+      ],
+    };
+    component.books = expectedBooks;
+    fixture.detectChanges();
+
+    // Act
+    const bookCardElement = debugElement.query(By.css('.genre-book'));
+    bookCardElement.triggerEventHandler('click', null);
+
+    // Assert
+    const expectedFormattedName = expectedBooks.booksList[0].name
+      .toLowerCase()
+      .replaceAll(' ', '-');
+    expect(mockRouter.navigate).toHaveBeenCalledWith([
+      '/details',
+      expectedFormattedName,
+    ]);
+  });
+
+  it('SHOULD navigate back WHEN back button is clicked', () => {
+    // Arrange
+    const expectedBooks: GenreBooks = {
+      genreName: 'Fiction',
+      booksList: [
+        {
+          name: 'Test Book',
+          image: 'test-image.jpg',
+          genre: ['Fiction'],
+          author: 'Test Author',
+          publishData: '',
+          price: 20,
+        },
+      ],
+    };
+    component.books = expectedBooks;
+    fixture.detectChanges();
+
+    // Act
+    const backButtonElement = debugElement.query(
+      By.css('.genre-container__back-button'),
+    );
+    backButtonElement.triggerEventHandler('click', null);
+
+    // Assert
+    expect(mockLocation.back).toHaveBeenCalled();
+  });
+
+  it('SHOULD update search results WHEN search query changes', () => {
+    // Arrange
+    const expectedSearchResults: SearchType = { query: 'test', results: [] };
+
+    // Act
+    fixture.detectChanges();
+    searchResultsSubject.next(expectedSearchResults);
+
+    // Assert
+    expect(component.results).toEqual(expectedSearchResults);
+  });
+
+  it('SHOULD update theme WHEN theme is toggled', () => {
+    // Arrange
+    const isLight = true;
+
+    // Act
+    fixture.detectChanges();
+    themeToggleSubject.next(isLight);
+
+    // Assert
+    expect(component.isLight).toBe(isLight);
+  });
+
+  it('SHOULD display the search component WHEN search query is not empty', () => {
+    // Arrange
+    component.results.query = 'test query';
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    const searchComponentElement = debugElement.query(By.css('app-search'));
+    expect(searchComponentElement).toBeTruthy();
+  });
+
+  it('SHOULD display genre books WHEN search query is empty', () => {
+    // Arrange
+    component.results.query = '';
+    component.books = {
+      genreName: 'Fiction',
+      booksList: [
+        {
+          name: 'Test Book',
+          image: 'test-image.jpg',
+          genre: ['Fiction'],
+          author: 'Test Author',
+          publishData: '',
+          price: 20,
+        },
+      ],
+    };
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    const bookCardElements = debugElement.queryAll(By.css('app-book-card'));
+    expect(bookCardElements.length).toBe(1);
   });
 });
--- phase11/book-store/src/app/components/genre-books/genre-books.component.ts
+++ phase11/book-store/src/app/components/genre-books/genre-books.component.ts
@@ -1,13 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {GroupByGenrePipe} from "../../pipes/group-by-genre.pipe";
-import {Location, NgForOf, NgOptimizedImage} from "@angular/common";
-import {GenreBooks} from "../../models/GenreBooks";
-import {Router, RouterLink} from "@angular/router";
-import {SearchType} from "../../models/SearchType";
-import {SearchComponent} from "../search/search.component";
-import {ThemeService} from "../../services/theme/theme.service";
-import {BookSearchService} from "../../services/search/book-search.service";
-import {BookCardComponent} from "../book-card/book-card.component";
+import { Component, Input, OnInit } from '@angular/core';
+import { GroupByGenrePipe } from '../../pipes/group-by-genre.pipe';
+import { Location, NgForOf, NgOptimizedImage } from '@angular/common';
+import { GenreBooks } from '../../models/GenreBooks';
+import { Router, RouterLink } from '@angular/router';
+import { SearchType } from '../../models/SearchType';
+import { SearchComponent } from '../search/search.component';
+import { ThemeService } from '../../services/theme/theme.service';
+import { BookSearchService } from '../../services/search/book-search.service';
+import { BookCardComponent } from '../book-card/book-card.component';
 
 @Component({
   selector: 'app-genre-books',
@@ -18,41 +18,45 @@ import {BookCardComponent} from "../book-card/book-card.component";
     NgOptimizedImage,
     RouterLink,
     SearchComponent,
-    BookCardComponent
+    BookCardComponent,
   ],
   templateUrl: './genre-books.component.html',
-  styleUrl: './genre-books.component.scss'
+  styleUrl: './genre-books.component.scss',
 })
-
 export class GenreBooksComponent implements OnInit {
-  @Input() books: GenreBooks = {genreName: '', booksList: []};
+  @Input() books: GenreBooks = { genreName: '', booksList: [] };
   results: SearchType = {
     query: '',
-    results: []
+    results: [],
   };
   isLight: boolean = false;
 
-  constructor(private location: Location, private router: Router, private seacrhService: BookSearchService, private themeService: ThemeService) {
-  }
+  constructor(
+    private location: Location,
+    private router: Router,
+    private seacrhService: BookSearchService,
+    private themeService: ThemeService,
+  ) {}
 
   goBack() {
     this.location.back();
   }
 
   goToDetails(name: string) {
-    this.router.navigate(['/details', name.toLowerCase().replaceAll(' ', '-')])
+    this.router
+      .navigate(['/details', name.toLowerCase().replaceAll(' ', '-')])
       .then(() => {
         return;
       });
   }
 
   ngOnInit(): void {
-    this.seacrhService.searchResults$.subscribe(output => {
+    this.seacrhService.searchResults$.subscribe((output) => {
       this.results = output;
     });
 
-  this.themeService.onToggle.subscribe(val => {
-    this.isLight = val;
-  });
+    this.themeService.onToggle.subscribe((val) => {
+      this.isLight = val;
+    });
   }
 }

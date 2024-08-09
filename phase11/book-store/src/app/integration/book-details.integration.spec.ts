8dbcd6032683d9767a2509a05269d6d2eef28de2
Arash-Azarpoor <azarpoor81@gmail.com>
2024-08-10 00:30:11 +0330

    Add a new test for book detail page

--- phase11/book-store/src/app/integration/book-details.integration.spec.ts
+++ phase11/book-store/src/app/integration/book-details.integration.spec.ts
@@ -1,19 +1,27 @@
-import {ComponentFixture, TestBed,} from '@angular/core/testing';
-import {of, Subject} from 'rxjs';
+import {ComponentFixture, fakeAsync, TestBed, tick,} from '@angular/core/testing';
+import {BehaviorSubject, of, Subject} from 'rxjs';
 import {Book} from '../models/Book';
 import {BookDetailsComponent} from "../components/book-details/book-details.component";
 import {BookProviderService} from "../services/book-provider/book-provider.service";
 import {BookOperationsService} from "../services/book-operation/book-operations.service";
 import {ConfirmationService, MessageService} from "primeng/api";
 import {ActivatedRoute, convertToParamMap} from "@angular/router";
+import {SearchType} from "../models/SearchType";
+import {By} from "@angular/platform-browser";
+import {BookSearchService} from "../services/search/book-search.service";
 
-describe('BookDetailsComponent', () => {
+describe('BookDetailsComponent Integration', () => {
   let component: BookDetailsComponent;
   let fixture: ComponentFixture<BookDetailsComponent>;
   let mockBookProviderService: jasmine.SpyObj<BookProviderService>;
   let mockBookOperationsService: jasmine.SpyObj<BookOperationsService>;
   let messageService: MessageService;
   let confirmationService: ConfirmationService;
+  let bookSearchServiceSpy: jasmine.SpyObj<BookSearchService>;
+  const searchResultsSubject = new BehaviorSubject<SearchType>({
+    query: '',
+    results: [],
+  });
 
   beforeEach(async () => {
     mockBookProviderService = jasmine.createSpyObj('BookProviderService', ['findBookByName']);
@@ -25,6 +33,9 @@ describe('BookDetailsComponent', () => {
         onUpdateBook: new Subject<Book>(),
       },
     );
+    bookSearchServiceSpy = jasmine.createSpyObj('BookSearchService', [], {
+      searchResults$: searchResultsSubject.asObservable(),
+    });
 
     messageService = new MessageService();
     confirmationService = new ConfirmationService();
@@ -48,21 +59,26 @@ describe('BookDetailsComponent', () => {
         {provide: BookProviderService, useValue: mockBookProviderService},
         {provide: BookOperationsService, useValue: mockBookOperationsService},
         {provide: ActivatedRoute, useValue: {paramMap: of(convertToParamMap({name: 'test-book-1'}))}},
+        { provide: BookSearchService, useValue: bookSearchServiceSpy },
         MessageService, ConfirmationService,
       ],
     }).compileComponents();
+
+    bookSearchServiceSpy = TestBed.inject(
+      BookSearchService,
+    ) as jasmine.SpyObj<BookSearchService>;
 
     fixture = TestBed.createComponent(BookDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should find and display the book details', () => {
+  it('SHOULD find and display the book details WHEN navigated to', () => {
     expect(component.book).toBeDefined();
     expect(component.book?.name).toEqual('Test Book 1');
   });
 
-  it('should delete the book and display success message', () => {
+  it('SHOULD delete the book and display success message WHEN called', () => {
     component.deleteConfirm({target: {}} as Event);
 
     expect(confirmationService.confirm).toHaveBeenCalled();
@@ -78,4 +94,31 @@ describe('BookDetailsComponent', () => {
       life: 3000,
     });
   });
+
+  it('SHOULD display search results WHEN new search results are emitted', fakeAsync(() => {
+    // Arrange
+    const mockSearchResults: SearchType = {
+      query: 'Dune',
+      results: [
+        {
+          name: 'Dune',
+          image: 'dune.jpg',
+          genre: ['Science Fiction'],
+          author: 'Frank Herbert',
+          publishData: '1965-08-01',
+          price: 9.99,
+        },
+      ],
+    };
+
+    // Act
+    searchResultsSubject.next(mockSearchResults);
+    tick();
+    fixture.detectChanges();
+
+    // Assert
+    expect(component.results).toEqual(mockSearchResults);
+
+    expect(fixture.debugElement.query(By.css('.search-books'))).toBeTruthy();
+  }));
 });
